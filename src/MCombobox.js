40e2132fb22370517ec24e9c4a54469d401b501f
viktorcpp <viktor.cpp.root@gmail.com>
2024-03-04 14:13:15 +0300

    full clean update

--- src/MCombobox.js
+++ src/MCombobox.js
@@ -1,128 +1,140 @@
+
 import React from 'react';
 
-class MComboOptionFake extends React.Component
-{
+class MComboOptionFake extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            value: props.value,
             index: props.index,
             text:  props.text,
         }
+
+        this.onOptionFakeClick = props.onclick;
+        this.native_el         = React.createRef();
     }
 
     render() {
         return (
-            <div className="select-fake-menu-item" data-index={this.state.index} onClick={this.props.onClick}>{this.state.text}</div>
-        );
-    }
-}
-
-class MComboOption extends React.Component
-{
-    constructor(props) {
-        super(props);
-
-        this.state =
-         {
-             value:props.value,
-             text :props.text,
-             selected: props.selected,
-         }
-    }
-
-    render()
-    {
-        return (
-            <option value={this.state.value} selected={this.state.selected}>{this.state.text}</option>
-        );
-    }
-}
-
-class MCombobox extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-
-        this.state = {
-            fake_combo_opened: false,
-            fake_combo_text: "FAKE",
-            data_list_options: [
-                { text:"Option 1"  },
-                { text:"Option 2"  },
-                { text:"Option 3"  },
-                { text:"Option 4"  },
-                { text:"Option 5"  },
-                { text:"Option 6"  },
-                { text:"Option 7"  },
-                { text:"Option 8"  },
-                { text:"Option 9"  },
-                { text:"Option 10" },
-            ],
-            selected: 0,
-        };
-
-        this.list_options      = [];
-        this.list_options_fake = [];
-
-        this.handleOnChangeMain    = this.handleOnChangeMain_.bind(this);
-        this.handleFakeOnClickMain = this.handleFakeOnClickMain_.bind(this);
-        this.handleFakeOnClickItem = this.handleFakeOnClickItem_.bind(this);
-
-        Object.keys(this.state.data_list_options).forEach( (key, index)=> {
-            this.list_options.push( <MComboOption value={key} text={this.state.data_list_options[index].text} /> );
-            this.list_options_fake.push( <MComboOptionFake text={this.state.data_list_options[index].text} index={index} onClick={this.handleFakeOnClickItem} /> )
-        });
-
-        this.setState( { fake_combo_text: this.list_options[this.state.selected].props.text } )
-    }
-
-    componentDidMount() {
-        //this.setState({ fake_combo_text: this.state.data_list_options[0].text });
-        this.setState( { fake_combo_text: this.list_options[this.state.selected].props.text } )
-    }
-
-    componentWillUnmount(){}
-
-    handleOnChangeMain_(e) {
-        let sel_index = e.target.selectedIndex;
-        this.setState({ fake_combo_text: this.state.data_list_options[sel_index].text });
-        this.setState( {selected: e.target.value} );
-    }
-
-    handleFakeOnClickMain_(e) {
-        this.setState({ fake_combo_opened : !this.state.fake_combo_opened });
-    }
-
-    handleFakeOnClickItem_(e) {
-        let indx = e.target.getAttribute("data-index");
-
-        this.setState( { fake_combo_text: this.list_options[indx].props.text } )
-        this.setState( { selected: indx } );
-    }
-
-    render()
-    {
-        return (
-            <div class="form-cont">
-                <form action="#" method="post">
-                <select name="select1" onChange={this.handleOnChangeMain} value={this.state.selected}>
-                    {this.list_options}
-                </select>
-                <div className={ this.state.fake_combo_opened ? "select-fake opened" : "select-fake" } onClick={this.handleFakeOnClickMain}>
-                    <span className="select-label">{this.state.fake_combo_text}</span>
-                    <span className="select-button"></span>
-                    <div className="select-fake-menu-holder">
-                        <div className="select-fake-menu">
-                            {this.list_options_fake}
-                        </div>
-                    </div>
-                </div>
-                </form>
+            <div class="select-fake-item" ref={this.native_el} value={this.state.value} index={this.state.index}
+                 onClick={this.onOptionFakeClick}>
+                {this.state.text}
             </div>
         );
     }
 }
 
-export default MCombobox;
+class MComboOption extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: props.value,
+            index: props.index,
+            text:  props.text,
+        }
+
+        this.native = React.createRef();
+    }
+
+    render() {
+        return (
+            <option value={this.state.value} index={this.state.index}>{this.state.text}</option>
+        );
+    }
+}
+
+export default class MCombobox extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selected: "",
+        }
+
+        this.data_options      = props.data_options;
+        this.list_options      = [];
+        this.list_options_fake = [];
+
+        this.onSelectChange         = this.onSelectChange_.bind(this);
+        this.onOptionFakeClick      = this.onOptionFakeClick_.bind(this);
+        this.setComboItemFakeActive = this.setComboItemFakeActive_.bind(this);
+        this.onComboFakeClick       = this.onComboFakeClick_.bind(this);
+        this.onBodyClickHide        = this.onBodyClickHide_.bind(this);
+
+        this.data_options.forEach( (val, index) => {
+            this.list_options.push( <MComboOption value={val} index={index} text={val} /> );
+            this.list_options_fake.push( <MComboOptionFake value={val} index={index} text={val} onclick={this.onOptionFakeClick} /> );
+        } );
+
+        this.native_el_ref    = React.createRef();
+        this.native_fake_val  = React.createRef();
+        this.native_fake_menu = React.createRef();
+        this.native_fake_main = React.createRef();
+    }
+
+    onComboFakeClick_(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.native_fake_main.current.classList.toggle("opened");
+        if( this.native_fake_main.current.classList.contains("opened") ) {
+            document.addEventListener( "click", this.onBodyClickHide );
+        }
+        else {
+            document.removeEventListener( "click", this.onBodyClickHide );
+        }
+    }
+
+    onOptionFakeClick_(e) {
+        let src_index = e.target.getAttribute("index");
+        let src_value = e.target.getAttribute("value");
+        this.native_fake_val.current.textContent = src_value;
+        this.setState({ selected: src_value });
+        this.native_el_ref.current.selectedIndex = src_index;
+
+        this.setComboItemFakeActive_(parseInt(src_index));
+    }
+
+    onSelectChange_(e) {
+        let selected_index = this.native_el_ref.current.selectedIndex;
+        let selected_val   = this.native_el_ref.current.options[selected_index].value;
+        this.setState({ selected: selected_val });
+        this.native_fake_val.current.textContent = selected_val;
+
+        this.setComboItemFakeActive(selected_index);
+    }
+
+    setComboItemFakeActive_(selected_index) {
+        this.native_fake_menu.current.childNodes.forEach( (val, index) => {
+            if( index === selected_index ) {
+                this.native_fake_menu.current.childNodes[index].classList.add("selected");
+            }
+            else {
+                this.native_fake_menu.current.childNodes[index].classList.remove("selected");
+            }
+        } );
+    }
+
+    onBodyClickHide_(e) {
+        Array.from( document.querySelectorAll(".select-fake.opened") ).forEach( function(val, index){
+            val.classList.remove("opened");
+        } );
+    }
+
+    render() {
+        return (
+            <div class="main">
+                <select value={this.state.selected} onChange={this.onSelectChange} ref={this.native_el_ref}>
+                    {this.list_options}
+                </select>
+                <div class="select-fake" ref={this.native_fake_main} onClick={this.onComboFakeClick}>
+                    <div class="select-fake-value" ref={this.native_fake_val}>Option 1</div>
+                    <div class="select-fake-menu" ref={this.native_fake_menu}>
+                        {this.list_options_fake}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MCombobox from './MCombobox';
 import './index.css';
+import MCombobox from './MCombobox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <MCombobox />
-    </React.StrictMode> 
+    <MCombobox data_options={
+        [   "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            "Option 5",
+            "Option 6",
+            "Option 7",
+            "Option 8",
+            "Option 9", ]
+    } />
 );
